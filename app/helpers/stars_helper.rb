a05fdec3a31848c1185696e82a0820350d571ba3
Michael Sabat <mike.sabat@gmail.com>
2018-05-05 21:56:51 -0400

    Add podcast via scaffold and paperclip images

--- app/helpers/stars_helper.rb
+++ app/helpers/stars_helper.rb
@@ -1,5 +1,7 @@
 module StarsHelper
 	def star_params
     params.require(:star).permit(:name, :bio, :link, :image)
+    #these star_params might not matter. We aren't using this with the podcast model and everything works. 
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502160314) do
+ActiveRecord::Schema.define(version: 20180506013701) do
+
+  create_table "podcasts", force: :cascade do |t|
+    t.string "name"
+    t.string "host"
+    t.string "feed"
+    t.text "desctiption"
+    t.string "homepage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "stars", force: :cascade do |t|
     t.string "name"
