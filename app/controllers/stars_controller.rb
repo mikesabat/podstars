4f96afb5630c954e352c80a480000c2edf96a80a
Michael Sabat <mike.sabat@gmail.com>
2018-05-20 16:50:28 -0400

    potential episodes list on star show page

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -13,7 +13,10 @@ class StarsController < ApplicationController
   # GET /stars/1.json
   def show
     @star = Star.find(params[:id])
+    puts @star.name
     @episodes = @star.episodes
+    puts "++++ episodes"
+    puts @episodes
     @episode = Episode.new
     @podcasts = Podcast.all
 
@@ -27,6 +30,40 @@ class StarsController < ApplicationController
     @choices.push(["Create New Podcast", nil]) 
 
     #above this shows how to manually enter a podcast episode with this star. Below, we want to query itunes for this star and display the resutls. 
+    #let's query and list all of the potential podcasts for this star. 
+    star_query = @star.name.strip.gsub(/\s+/, "+")
+
+     response = HTTParty.get "https://listennotes.p.mashape.com/api/v1/search?len_min=10&offset=6&only_in=title&published_after=0&q=#{star_query}&sort_by_date=0&type=episode",
+        headers:{
+        'X-Mashape-Key' => ENV['MASHAPE_KEY'],
+        "Accept" => "application/json"
+      }
+
+      num_of_results = response["count"]
+      @episodes_arr = response["results"]
+      puts num_of_results
+      puts @episodes_arr
+    
+                # The method and while loop are attempts to collect all possible episodes for the Star
+                # def all_episodes(star, offset) 
+                    
+                #     @response = HTTParty.get "https://listennotes.p.mashape.com/api/v1/search?len_min=10&offset=#{offset}&only_in=title&published_after=0&q=#{star}&sort_by_date=0&type=episode",
+                #    headers:{
+                #    'X-Mashape-Key' => ENV['MASHAPE_KEY'],
+                #    "Accept" => "application/json"
+                #  }
+                #     all_eps.push(@response["results"])
+                #     offset += 10
+                # end
+
+                # offset = 0
+                # all_eps = []
+
+                # while num_of_results != 0 
+                #   all_episodes(star_query, offset) 
+                #   puts"--------"
+                #   puts all_eps         
+                # end
   end
 
   # GET /stars/new
@@ -53,7 +90,7 @@ class StarsController < ApplicationController
      #searching the API for this star
      puts @response
      num_of_results = @response["count"]
-     puts num_of_results
+
 
      @episodes_arr = @response["results"]
 
@@ -79,9 +116,6 @@ class StarsController < ApplicationController
   # POST /stars
   # POST /stars.json
   def create
-    g = params[:name]
-    puts "+++++++"
-    puts g
     @star = Star.new(star_params)
     #(:name => params[:name]) 
     #I'm sidestepping the Strong parameters. My gut says that I'm not naming the parameters correctly coming from the link.
@@ -102,8 +136,9 @@ class StarsController < ApplicationController
   def update
     respond_to do |format|
       if @star.update(star_params)
-        format.html { redirect_to @star, notice: 'Star was successfully updated.' }
+        format.html { redirect_to @star, notice: 'Star was successfully updated.' } #maybe now is when we need to compile the episodes and show on @star page.
         format.json { render :show, status: :ok, location: @star }
+        puts @star
       else
         format.html { render :edit }
         format.json { render json: @star.errors, status: :unprocessable_entity }
